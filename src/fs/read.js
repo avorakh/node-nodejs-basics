afa90e5b9b77992d5215886fccf8fa5fddf52c8e
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-04 21:59:07 +0200

    Add list.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,4 +1,8 @@
-const read = async () => {
+import { join } from 'path';
+import { readdir } from 'fs/promises';
+import { currentDirectoryPath, isExistingPath } from './utils.js'
+
+const read = async () => {fileToRead.txt
     // Write your code here 
 };
 
