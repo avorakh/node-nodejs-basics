72788531927295bdcc7bc51c2e270f1237dd7777
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-04 21:43:21 +0200

    Add rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { join } from 'path';
+import { rename as rnm } from 'fs/promises';
+import { currentDirectoryPath, isExistingPath } from './utils.js'
+
+
 const rename = async () => {
-    // Write your code here 
+    let currentDir = currentDirectoryPath();
+
+    let oldFilePath = join(currentDir, 'files', 'wrongFilename.txt');
+    let newFilePath = join(currentDir, 'files', 'properFilename.md');
+
+    let isOldFilePathExist = await isExistingPath(oldFilePath);
+    let isNewFilePathExist = await isExistingPath(newFilePath);
+
+    if (isOldFilePathExist && !isNewFilePathExist) {
+        await rnm(oldFilePath, newFilePath)
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
