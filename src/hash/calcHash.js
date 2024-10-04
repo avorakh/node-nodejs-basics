59b36e805abf4750f1b97e10b6270cc0377ad43a
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-05 01:03:12 +0200

    Modify hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createHash } from 'crypto';
+import { join } from 'path';
+import { open } from 'node:fs/promises';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+    let filePath = join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    let fd = await open(filePath);
+    let stream = fd.createReadStream();
+
+    let hash = createHash('sha256');
+
+    stream.on('error', function (error) {
+
+        console.log(`error: ${error.message}`);
+    })
+
+    stream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    stream.on('end', () => {
+        let result = hash.digest('hex');
+        console.log(result);
+    });
 };
 
 await calculateHash();
