311e7b8ec77cfab2d31f449916d511142c90b73f
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-04 21:50:03 +0200

    Add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { join } from 'path';
+import { rm } from 'fs/promises';
+import { currentDirectoryPath, isExistingPath } from './utils.js'
+
+
 const remove = async () => {
-    // Write your code here 
+    let currentDir = currentDirectoryPath();
+    let filePath = join(currentDir, 'files', 'fileToRemove.txt');
+
+    let isExistingFile = await isExistingPath(filePath);
+
+    if (isExistingFile) {
+        await rm(filePath);
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
