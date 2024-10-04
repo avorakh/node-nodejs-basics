f5ea1fa40e8d9fdf14400c4cb49a255c19263ebf
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-04 22:03:49 +0200

    Add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,9 +1,20 @@
 import { join } from 'path';
-import { readdir } from 'fs/promises';
+import { readFile } from 'fs/promises';
 import { currentDirectoryPath, isExistingPath } from './utils.js'
 
-const read = async () => {fileToRead.txt
-    // Write your code here 
+
+const read = async () => {
+    let currentDir = currentDirectoryPath();
+    let filePath = join(currentDir, 'files', 'fileToRead.txt');
+
+    let isExistingFile = await isExistingPath(filePath);
+
+    if (isExistingFile) {
+        let fileContent = await readFile(filePath, { encoding: 'utf8' });
+        console.log(fileContent);
+    } else {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
