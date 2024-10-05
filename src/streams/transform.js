b4525e92fb5e552ee856791f0d4a31cccca6aabc
Aliaksandr Vorakh <aliaksandrvorakh@gmail.com>
2024-10-05 03:41:15 +0200

    Modify Transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { pipeline } from 'stream/promises';
+import { Transform } from 'stream';
+import { stdin, stdout } from 'process';
+
 const transform = async () => {
-    // Write your code here 
+
+    const reverseTextStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversedChunk = chunk.toString().split('').reverse().join('');
+            callback(null, reversedChunk);
+        }
+    });
+
+    await pipeline(
+        stdin,
+        reverseTextStream,
+        stdout
+    );
 };
 
 await transform();
